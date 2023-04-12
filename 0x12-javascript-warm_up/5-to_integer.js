6ef3c58e621720a942e0ca3d0467b8a9dc392eca
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-12 03:05:12 +0000

    task 5 re-done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const variable = process.argv[2];
-if (isNaN(variabl)){
-	console.log('Not a number');
-}
-else{
-	console.log('My number:', value | 0);
+if (isNaN(variabl)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', value | 0);
 }
