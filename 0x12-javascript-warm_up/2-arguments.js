fd5770c5348573bd5638f7521ee0de0793feea97
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-12 01:45:57 +0000

    task 2 done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-/*Commandline arguments in javascript use the process.argv
-which contains arguments in the CLI.from process.argv[0] - 
-process.argv[2] being the user paseed in the arguments.
-*/
 if (process.argv.length < 3) {
 	console.log('No argument');
 }
@@ -11,3 +7,4 @@ else if (process.argv.length === 3){
 }
 else {
 	console.log('Arguments found');
+}
