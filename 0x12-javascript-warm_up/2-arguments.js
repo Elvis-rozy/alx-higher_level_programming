77cac0e517dca59400a47892500b50394cc40d0a
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-12 01:52:15 +0000

    task 2 done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+/*
+Commandline arguments in js is use the process.argv
+which is an array containing all arguments of the CLI.
+process.argv[0] = absolute path to the node binary file
+process.argv[1] = absolute file path to the .js fie
+process.argv[2] = users passed in arguments.
+*/
 if (process.argv.length < 3){
 	console.log('No argument');
 }
