3593569be2d0f6e21167f57e3e91778f116a9ff8
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-12 16:46:01 +0000

    advanced task 1 attempt 4

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const theList = require('./100-data.js').list;
 console.log(theList);
-const secondList = theList.map((x, index) => x * index);
+let times = 0;
+const secondList = theList.map(function (x) {
+  return (x * times++);
+});
 console.log(secondList);
