ab24b7072ee8f79f11f3b597e13e4278e2337e15
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-27 13:37:03 +0000

    advanced task 2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send JSON POST request to URL passed as first argument and displays the response
-curl -s -H "Content-Type: application/json" -d @"$2" "$1"
+curl -sX POST -d "@$2" -H "Content-Type: application/json" "$1"
