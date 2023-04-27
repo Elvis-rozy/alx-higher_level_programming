b4a1285d9293ae3d8ef50cb08ccc33de274f9c2b
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-27 12:34:08 +0000

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
 finds a peak(highest) in a list of unsorted integers
-
 """
+
 def find_peak(list_of_integers):
+    """
+    finds the peak in the array
+    """
     if bool(list_of_integers) is False:
         return None
     list_of_integers.sort()
