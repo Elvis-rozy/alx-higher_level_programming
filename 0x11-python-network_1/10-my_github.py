66d8ad4f63b64edfa1822e0dade1c4d8a8285bd0
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-28 08:27:48 -0700

    Task 100 re-tried

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""Script that:
-- takes your GitHub credentials (username and password)
-- uses the GitHub API to display your id
 """
-import sys
+Module to access to the GitHub API and uses the information
+"""
 import requests
 from requests.auth import HTTPBasicAuth
+from sys import argv
 
+
+def main(argv):
+    """
+    Script takes your GitHub credentials (username and password) and
+    uses the GitHub API to display your id.
+    """
+    user = argv[1]
+    password = argv[2]
+    response = requests.get('https://api.github.com/user',
+                            auth=HTTPBasicAuth(user, password))
+    try:
+        profile_information = response.json()
+        print(profile_information['id'])
+    except:
+        print('None')
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    reque = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    main(argv)
