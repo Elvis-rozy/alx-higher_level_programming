5251b5c8c8461dcad87a2e6243512c09e36574c7
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-28 07:29:10 -0700

    Task 2 re-tried

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
+"""Script that takes in a URL and an email
 - sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
+- with the email as a parameter
+- displays the body of the response (decoded in utf-8)
 """
 import sys
 import urllib.parse
@@ -12,8 +11,8 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    content = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(content).encode("ascii")
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
