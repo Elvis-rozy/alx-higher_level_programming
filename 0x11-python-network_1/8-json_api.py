e9976e0a0dedf98f17402fbcd7f8f3edeff86508
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-28 08:14:06 -0700

    Task 8 re-tried

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script tha:
+"""Script that:
 - takes in a letter
 - sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
@@ -9,12 +9,12 @@ import requests
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    letr = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload_taken = {"q": letr}
 
-    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    reque = requests.post("http://0.0.0.0:5000/search_user", data=payload_taken)
     try:
-        response = req.json()
+        response = reque.json()
         if response == {}:
             print("No result")
         else:
