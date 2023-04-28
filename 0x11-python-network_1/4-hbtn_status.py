c2703ec61d2e20b950d96bb7489c164e04756b05
Elvis-rozy <elvisrozy52@gmail.com>
2023-04-28 07:46:33 -0700

    Task 4 re-tried

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+
+if __name__ == "__main__":
+    reque = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(reque.text)))
+    print("\t- content: {}".format(reque.text))
