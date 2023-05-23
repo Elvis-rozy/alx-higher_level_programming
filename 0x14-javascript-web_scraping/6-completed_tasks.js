e9b764bd95b0be77b165589b8d747cfa678e37f6
Elvis-rozy <elvisrozy52@gmail.com>
2023-05-23 15:08:12 +0000

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,16 @@
 #!/usr/bin/node
-// Script that computes the number of tasks completed by user id.
-
-const argmts = process.argv;
-let requestURL = args[2];
-let request = require('request');
-request(requestURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    let todo = JSON.parse(body);
-    let dash = {};
-    for (let i = 0; i < todo.length; i++) {
-      let status = (todo[i]['completed']);
-      let key = todo[i]['userId'].toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
-        }
+const req = require('request');
+req(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const complete = {};
+    todos.forEach((todo) => {
+      if (todo.complete && complete[todo.userId] === undefined) {
+        complete[todo.userId] = 1;
+      } else if (todo.complete) {
+        complete[todo.userId] += 1;
       }
-    }
-    console.log(dash);
+    });
+    console.log(complete);
   }
 });
