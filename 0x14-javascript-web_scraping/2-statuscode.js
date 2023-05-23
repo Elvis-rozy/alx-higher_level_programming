ecef10d65a9c51fd615192fd13563076d5ac691d
Elvis-rozy <elvisrozy52@gmail.com>
2023-05-23 14:59:14 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-// Script that display the status code of a GET request
-
-const args = process.argv;
-let request = require('request');
-request(args[2], function (error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else console.log('code:', response && response.statusCode);
+const req = require('request');
+req.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
